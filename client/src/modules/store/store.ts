cf50abb222490c340a8874566986274ac53eee87
Julie <julie.trucvallet@gmail.com>
2026-04-16 23:05:01 +0200

    fix test store reducer warning

--- client/src/modules/store/store.ts
+++ client/src/modules/store/store.ts
@@ -2,7 +2,11 @@ import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import { useDispatch } from "react-redux";
 import type { Dependencies } from "./dependencies";
 
-const reducers = combineReducers({})
+const appReducer = (state = {}) => state;
+
+const reducers = combineReducers({
+  app: appReducer,
+});
 
 export type AppStore = ReturnType<typeof createStore>;
 export type AppState = ReturnType<typeof reducers>;
@@ -10,20 +14,21 @@ export type AppDispatch = AppStore["dispatch"];
 export type AppGetState = AppStore["getState"];
 
 export const createStore = (config: {
-    dependencies: Dependencies
+  dependencies: Dependencies;
 }) => {
-    const store = configureStore({
-        reducer: reducers,
-        devTools: true,
-        middleware: (getDefaultMiddleware) => {
-            return getDefaultMiddleware({
-                thunk: {
-                    extraArgument: config.dependencies
-                }
-            })
-        }
-    })
-    return store;
-}
+  const store = configureStore({
+    reducer: reducers,
+    devTools: true,
+    middleware: (getDefaultMiddleware) => {
+      return getDefaultMiddleware({
+        thunk: {
+          extraArgument: config.dependencies,
+        },
+      });
+    },
+  });
+
+  return store;
+};
 
 export const useAppDispatch = () => useDispatch<AppDispatch>();
